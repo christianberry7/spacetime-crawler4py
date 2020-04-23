e367cc91dc2998dc16b80561d528cb07b1e3bae8
unknown <btyang24@gmail.com>
2020-04-23 12:31:48 -0700

    fixed syntax of regex serach for files we dont want

--- scraper.py
+++ scraper.py
@@ -92,8 +92,8 @@ def is_valid(url):
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$"
-            + r"|ppsx|pdf", parsed.path.lower())    
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz"
+            + r"|ppsx)$", parsed.path.lower())    
 
     except TypeError:
         print ("TypeError for ", parsed)
