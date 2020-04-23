7b37accfe33eec2101a90db96d35a4951c8b0df5
unknown <btyang24@gmail.com>
2020-04-22 17:50:16 -0700

    Crawler now ignores href with attr rel=r'nofollow|ugc|sponsered'

--- scraper.py
+++ scraper.py
@@ -11,6 +11,8 @@ LEGAL_DOMAINS = re.compile(_LEGAL_DOMAINS)
 _TRUNCATE = r'[#].*'
 TRUNCATE = re.compile(_TRUNCATE)
 
+NO_CRAWL_REL = re.compile(r'nofollow|ugc|sponsered')
+
 
 def scraper(url, resp):
     # Need to handle redirection loops
@@ -20,20 +22,35 @@ def scraper(url, resp):
         return []
 
     links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+    return [link for link in links]
 
 def extract_next_links(url, resp):
     soup = bs4.BeautifulSoup(resp.raw_response.content, 'html.parser')
     
-    all_links = [(link.get('href')) for link in soup.find_all('a', attrs={'href': re.compile(r'^http[s]://')})]
+    all_links = [link.get('href') for link in soup.find_all(is_tag_crawlable)]
     legal_links = list(filter(is_legal_and_valid, all_links))
     legal_links = set(map(truncate_fragment, legal_links))
+
+    count = 0
+    with open('word_desnity_log.txt', 'a') as file:
+        for word in soup.get_text().split():
+            count += len(word)
+        file.write(str(count) + '\n')
 
     # Debugging purposes
     # print(">> Found all links:\n>> " + "\n>> ".join(all_links))
     # print(">> Returning legal links:\n>> " + "\n>> ".join(legal_links))
     
     return legal_links
+
+def is_tag_crawlable(tag):
+    if tag.name == 'a':
+        if tag.has_attr('rel'):
+            # print(tag.attrs)
+            for attr in tag['rel']:
+                if NO_CRAWL_REL.search(attr) is None:
+                    return True
+    return False
 
 def is_legal_and_valid(url):
     # Checks to see if URL is within our intended scope
